b6bf6ecde4de6d3d85a434a50cf73c74cae74084
douglasmk <douglasmk@users.noreply.github.com>
2021-04-21 18:02:23 -0300

    Aula02 (#3)
    * Implementação do sqlite, orm, migrations e rota de criação de settings
    
    * adicionado sonar
    
    * desabilitado x-powered-by

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,13 @@
 import express from "express";
+import "./database"
+import { routes } from "./routes";
 
 const app = express();
 
-app.get("/", (request, response) => {
-    return response.json({
-        message: "Olá NLW 05!"
-    });
-})
+app.disable("x-powered-by"); // recomendação do sonar
 
-app.post("/users", (request, response) => {
-    return response.json({
-        message: "Usuário salvo com sucesso!"
-    });
-})
+app.use(express.json());
+
+app.use(routes);
 
 app.listen(3000, () => console.log("Server is running on port 3000"));
